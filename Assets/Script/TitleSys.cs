c1dc47fff799f1d5b7a48b47d0097d3ea7b022e5
TUNANOTE\kutsu <crop.llc.jp@gmail.com>
2026-01-13 10:30:04 +0900

    タイトルシステム追加 久綱

--- Assets/Script/TitleSys.cs
+++ Assets/Script/TitleSys.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class GameSys : MonoBehaviour
+public class TitleSys : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
